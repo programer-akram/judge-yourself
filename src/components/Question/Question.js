d2a370c0c75532b03de071a94e6188e2e3c983a1
Programer-akram <mah.akram202@gmail.com>
2022-10-25 04:45:33 +0600

    questions of quiz selected

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,51 +1,51 @@
 
-// import React, { useState } from 'react';
-// import Option from '../Option/Option';
-// import './Question.css';
+import React, { useState } from 'react';
+import Option from '../Option/Option';
+import './Question.css';
 
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-
-// const Question = ({question, index}) => {
-//     const [answer, setAnswer] = useState([]);
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
-//     const singleQuestion = question.question;
-//     singleQuestion.slice(0,3);
+const Question = ({question, index}) => {
+    const [answer, setAnswer] = useState([]);
 
-//     const handleAnswer = () => {
-//         setAnswer(question.correctAnswer); 
-//         toast.success( answer);   
+
+    const singleQuestion = question.question;
+    singleQuestion.slice(0,3);
+
+    const handleAnswer = () => {
+        setAnswer(question.correctAnswer); 
+        toast.success( answer);   
         
-//     }
-//     const handleOption = (val) => {
-//         if(val === question.correctAnswer){
-//             toast.success("Correct Answer");
+    }
+    const handleOption = (val) => {
+        if(val === question.correctAnswer){
+            toast.success("Correct Answer");
             
             
             
-//         }else {
-//             toast.error("Wrong Answer");
+        }else {
+            toast.error("Wrong Answer");
             
-//         }
-//     }
+        }
+    }
 
-//     return (
-//         <div className='sigle-question'>
-//             <h5>Quiz {index+1}. {singleQuestion.replace(/(<([^>]+)>)/ig, '')}</h5>
+    return (
+        <div className='sigle-question'>
+            <h5>Quiz {index+1}. {singleQuestion.replace(/(<([^>]+)>)/ig, '')}</h5>
             
-//             <div className='radio_section'>
-//                 {
-//                 question.options.map(option => {
-//                     return <Option key={option.id} option={option} handleOption={handleOption} question={question} handleAnswer={handleAnswer}></Option>
-//                 })
-//                 }
-//                 <ToastContainer />
-//             </div>
+            <div className='radio_section'>
+                {
+                question.options.map(option => {
+                    return <Option key={option.id} option={option} handleOption={handleOption} question={question} handleAnswer={handleAnswer}></Option>
+                })
+                }
+                <ToastContainer />
+            </div>
 
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default Question;
+export default Question;
