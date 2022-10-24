f7433d5d076a936feb5aaf116756bc905ab3084b
Programer-akram <mah.akram202@gmail.com>
2022-10-25 04:53:01 +0600

    showing correct answer toast added

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,17 +1,17 @@
-// import { faEye } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// const Option = ({option,question,handleAnswer,handleOption}) => {
-//     console.log(option);
-//     return (
-//         <div>
+const Option = ({option,question,handleAnswer,handleOption}) => {
+    console.log(option);
+    return (
+        <div>
             
-//             <Link onClick={()=>handleAnswer()}><FontAwesomeIcon className='eye' icon={faEye}></FontAwesomeIcon></Link>
-//             <input onClick={(e)=>handleOption(e.target.value)} type="radio" name={question.id} id={option} value={option}/> {option}
-//         </div>
-//     );
-// };
+            <Link onClick={()=>handleAnswer()}><FontAwesomeIcon className='eye' icon={faEye}></FontAwesomeIcon></Link>
+            <input onClick={(e)=>handleOption(e.target.value)} type="radio" name={question.id} id={option} value={option}/> {option}
+        </div>
+    );
+};
 
-// export default Option;
+export default Option;
